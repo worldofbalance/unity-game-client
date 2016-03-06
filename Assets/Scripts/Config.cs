51a658f91c93471fde7f8463c4ce7a1ced5fbd6b
Nicu Listana <niculistana@gmail.com>
2016-03-05 16:42:32 -0800

    Changed config IP
    Previous remote host is no longer working, so I replaced it with
    IP:52.32.228.220

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -6,5 +6,5 @@ public class Config {
 	//public static readonly string REMOTE_HOST = "52.25.138.18";
 	//public static readonly string REMOTE_HOST = "thecity.sfsu.edu";
 	//public static readonly string REMOTE_HOST = "smurf.sfsu.edu";
-	public static readonly string REMOTE_HOST = "54.69.245.208";
+	public static readonly string REMOTE_HOST = "52.32.228.220";
 }
