6bec33c5c401c40b053ebff20f8dfeb7600df9a2
Fito von Zastrow <fito@paragil.com>
2016-03-07 21:10:32 -0800

    Refactor:  - Uncomments zone generation in EcosystemController to reactivate Ecosystem gameplay.  - Changes tabs to spaces for all the scripts in the Scripts/Ecosystem/    directory.

--- Assets/Scripts/Ecosystem/EcosystemMouse.cs
+++ Assets/Scripts/Ecosystem/EcosystemMouse.cs
@@ -4,62 +4,62 @@ using System.Collections;
 
 public class EcosystemMouse : MonoBehaviour {
 
-	public string type = "";
-	public GameObject roamingCursor;
-	public Zone zone { get; set; }
-	
-	// Use this for initialization
-	void Start() {
-		roamingCursor.GetComponent<Renderer>().material.color = new Color32(0, 181, 248, 255);
-		roamingCursor.transform.localScale *= Constants.ECO_HEX_SCALE;
-	}
-	
-	// Update is called once per frame
-	void Update() {
-		if (Input.GetKeyDown("1")) {
-			type = "African Elephant";
-		}
+    public string type = "";
+    public GameObject roamingCursor;
+    public Zone zone { get; set; }
 
-		if (Input.GetKeyDown("2")) {
-			type = "Decaying Material";
-		}
+    // Use this for initialization
+    void Start() {
+        roamingCursor.GetComponent<Renderer>().material.color = new Color32(0, 181, 248, 255);
+        roamingCursor.transform.localScale *= Constants.ECO_HEX_SCALE;
+    }
 
-		if (Input.GetKeyDown(KeyCode.Escape)) {
-			type = "";
-		}
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("1")) {
+            type = "African Elephant";
+        }
 
-		if (type == "") {
-			roamingCursor.SetActive(false);
-		} else {
-			roamingCursor.SetActive(true);
-		}
+        if (Input.GetKeyDown("2")) {
+            type = "Decaying Material";
+        }
 
-		if (roamingCursor.activeInHierarchy) {
-			RaycastHit hit = new RaycastHit();
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            type = "";
+        }
 
-			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
-				zone = hit.transform.gameObject.GetComponent<Zone>();
+        if (type == "") {
+            roamingCursor.SetActive(false);
+        } else {
+            roamingCursor.SetActive(true);
+        }
 
-				roamingCursor.transform.position = hit.transform.gameObject.transform.position + new Vector3(0, 0.1f, 0);
-			}
+        if (roamingCursor.activeInHierarchy) {
+            RaycastHit hit = new RaycastHit();
 
-			if (Input.GetMouseButtonDown(0)) {
-				roamingCursor.SetActive(false);
-				Create(type, roamingCursor.transform.position);
-				type = "";
-			}
-		}
-	}
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+                zone = hit.transform.gameObject.GetComponent<Zone>();
 
-	void OnGUI() {
-		if (roamingCursor.activeInHierarchy) {
-			GUIExtended.Label(new Rect((Screen.width - 100) / 2, Screen.height * 0.75f, 100, 100), "Choose Location", new GUIStyle(GUI.skin.label), Color.black, Color.white);
-		}
-	}
+                roamingCursor.transform.position = hit.transform.gameObject.transform.position + new Vector3(0, 0.1f, 0);
+            }
 
-	public void Create(string type, Vector3 position) {
-		GameObject species = Instantiate(Resources.Load("Prefabs/" + type)) as GameObject;
-		species.transform.position = position + new Vector3(0, 0.1f, 0);
-		species.transform.GetChild(0).localEulerAngles = new Vector3(40, 180, 0);
-	}
+            if (Input.GetMouseButtonDown(0)) {
+                roamingCursor.SetActive(false);
+                Create(type, roamingCursor.transform.position);
+                type = "";
+            }
+        }
+    }
+
+    void OnGUI() {
+        if (roamingCursor.activeInHierarchy) {
+            GUIExtended.Label(new Rect((Screen.width - 100) / 2, Screen.height * 0.75f, 100, 100), "Choose Location", new GUIStyle(GUI.skin.label), Color.black, Color.white);
+        }
+    }
+
+    public void Create(string type, Vector3 position) {
+        GameObject species = Instantiate(Resources.Load("Prefabs/" + type)) as GameObject;
+        species.transform.position = position + new Vector3(0, 0.1f, 0);
+        species.transform.GetChild(0).localEulerAngles = new Vector3(40, 180, 0);
+    }
 }
