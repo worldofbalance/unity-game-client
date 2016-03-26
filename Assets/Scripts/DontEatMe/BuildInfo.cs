dd3e3cc6dfbdafdbc0838f023ad766b5f0b039c7
Jeremy <jeremy@Azel.local>
2016-03-25 21:50:52 -0700

    Modified 'BuildInfo.cs': made 'speciesId' variable public to enable instance assignment

--- Assets/Scripts/DontEatMe/BuildInfo.cs
+++ Assets/Scripts/DontEatMe/BuildInfo.cs
@@ -6,17 +6,20 @@ public class BuildInfo : MonoBehaviour {
 	// the menu button sprite
 	public Texture previewImage;
 
-	// Current Health
-	[SerializeField]
-	int speciesId = 0;
-
-
-  public int GetSpeciesId() {
-		return speciesId;
-	}
+	// Species ID
+	// NOTE: this is made public for the time being to allow assignment of species ID to GameObject instances
+	public int speciesId;
 
 	// the cost to build the item
 	public int price;
+
+	// Current Health
+	//[SerializeField]
+	//int speciesId = 0;
+
+  	public int GetSpeciesId() {
+		return speciesId;
+	}
 
 	// Use this for initialization
 	void Start () {
