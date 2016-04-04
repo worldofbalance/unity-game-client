f49a14b94e27a2234a35a1427a4d9f8b914a1868
Dusan Cvetkovic <cvdusan@yahoo.com>
2016-04-03 23:41:56 -0700

    change remote_host to localhost, so that it works for other team members. (Note: when testing on phone if server is ran locally, you need to specify ip address to the local server)

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class Config
 {
-    //    public static readonly string REMOTE_HOST = "localhost";
+    public static readonly string REMOTE_HOST = "localhost";
     //public static readonly string REMOTE_HOST = "52.25.138.18";
     //public static readonly string REMOTE_HOST = "thecity.sfsu.edu";
     //public static readonly string REMOTE_HOST = "smurf.sfsu.edu";
     //	public static readonly string REMOTE_HOST = "52.32.228.220";
     //home
-    public static readonly string REMOTE_HOST = "192.168.0.18";
+    //    public static readonly string REMOTE_HOST = "192.168.0.18";
     //school
     //    public static readonly string REMOTE_HOST = "10.143.63.199";
 
