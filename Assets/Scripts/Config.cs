f2b4eb64cf7c9344e698ef95835d3be752eadebc
Jason Huang <narykx@hotmail.com>
2016-04-11 15:32:09 -0700

    updated convergence UI

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -2,10 +2,10 @@
 using UnityEngine;
 
 public class Config {
-	//public static readonly string REMOTE_HOST = "localhost";
+	public static readonly string REMOTE_HOST = "localhost";
 	//public static readonly string REMOTE_HOST = "52.25.138.18";
 	//public static readonly string REMOTE_HOST = "thecity.sfsu.edu";
 	//public static readonly string REMOTE_HOST = "smurf.sfsu.edu";
 	//public static readonly string REMOTE_HOST = "130.212.93.116";
-	public static readonly string REMOTE_HOST = "52.32.228.220";
+	//public static readonly string REMOTE_HOST = "52.32.228.220";
 }
