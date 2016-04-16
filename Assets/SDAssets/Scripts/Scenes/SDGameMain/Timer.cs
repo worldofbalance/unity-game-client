3d80743697d752dd92d7ab243d970a2d357e45b2
Toru Nagao <toru@Toru-no-MacBook-Pro.local>
2016-04-16 01:07:48 -0700

    Renamed TimerScore.cs to Timer.cs, and modified the script so the game ends when the time runs out and go back to SDReadyScene(for now)

---
+++ Assets/SDAssets/Scripts/Scenes/SDGameMain/Timer.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * File Name: TimerScore.cs
+ * Description: Script for TimeText. This sciprt works as a timer.
+ */
+
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace SD {
+public class Timer : MonoBehaviour {
+
+    public Text timeText;
+    public float time = 180f;
+
+    public void Start(){
+        timeText.text = "Time: " +time.ToString ();
+    }
+
+    void Update() {
+        time -= 1f * Time.deltaTime;
+        timeText.text = "Time: " +((int)time).ToString ();
+        if (time <= 0) {
+                timeText.text = "Time's Up!";
+                if (time <= -3) {
+                    Game.SwitchScene ("SDReadyScene");
+                }
+        }
+    }
+
+        float GetTime(){
+            return this.time;
+        }
+}
+}
