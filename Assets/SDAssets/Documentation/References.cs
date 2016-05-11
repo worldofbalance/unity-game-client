ff1a35408457e4e35a27a8b6f1751f926a0f0c50
Toru Nagao <toru@Toru-no-MacBook-Pro.local>
2016-05-11 09:12:25 -0700

    Added sample BGM and SEs, and updated the reference file

--- Assets/SDAssets/Documentation/References.cs
+++ Assets/SDAssets/Documentation/References.cs
@@ -1,10 +1,17 @@
 ﻿/*
 
-This file contains the information about references to free images that we used for 3D fish models
+This file contains the information about references to materialsthat we used for Sea Divided
+
+Free Images
 1, TitleShark.png - Public Domain https://openclipart.org/detail/222313/shark-silhouette
 2, SmallFish1.png - Public Domain https://openclipart.org/detail/243725/zoster-wrasse
 3, SmallFish2.png - Public Domain https://openclipart.org/detail/241908/blackbanded-seaperch
 4, SmallFish3.png - Public Domain https://openclipart.org/detail/241907/eastern-wirrah
 5, RedFish.png, Herring.png, Mackerel.png - Public Domain http://all-free-download.com/free-vector/download/marine_fish_vector_295711.html
 
+
+Free Sound Effects and BGM
+1, bgm.mp3     - http://www.orangefreesounds.com/nature-music/
+2, consume.mp3 - http://www.flashkit.com/soundfx/Ambience/Woop-Ryan_Cur-8800/index.php
+3, damage.mp3  - http://soundbible.com/2069-Realistic-Punch.html
  */ 
