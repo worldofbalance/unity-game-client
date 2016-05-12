844a627201e6c2c0ace9544683c08802bbe37b53
Fito von Zastrow <fito@paragil.com>
2016-05-12 13:20:03 -0700

    Changes port to point to lobby's remote server

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -17,7 +17,7 @@ public class Constants
     // public static readonly string REMOTE_HOST = "thecity.sfsu.edu";
     // IP address may be 130.212.3.51
 
-    public static readonly int REMOTE_PORT = 9255;
+    public static readonly int REMOTE_PORT = 9234;
     //20041;
     // public static readonly int REMOTE_PORT = 9257;    //9257 9255 for locahost, 9257 for Anu's server
 
