90df6b63d2990d09353ef63220a605a6bbb5272e
Dusan Cvetkovic <cvdusan@yahoo.com>
2016-05-15 18:22:55 -0700

    a bit of refactoring (input controller) cleanup - removed some of the unnecessary files

--- Assets/Scripts/ClashOfSpecies/COSMobileInputControler.cs
+++ Assets/Scripts/ClashOfSpecies/COSMobileInputControler.cs
@@ -7,13 +7,14 @@ using UnityEngine.EventSystems;
 
 
 
-public class COSTouchInputControler:InputControllerBase
+public class COSMobileInputControler:COSAbstractInputController
 {
     
 
-    public COSTouchInputControler () : base ()
+    public COSMobileInputControler()
+        : base()
     {
-        walkableAreaMask = (int)Math.Pow (2, NavMesh.GetAreaFromName ("Walkable"));
+        walkableAreaMask = (int)Math.Pow(2, NavMesh.GetAreaFromName("Walkable"));
     }
 
     //    COSTouchState eTouchRes;
@@ -54,35 +55,9 @@ public class COSTouchInputControler:InputControllerBase
     private Vector3 scrollDirection = Vector3.zero;
     private Vector3 oldTouchPos;
 
-    public override void InputControllerAwake (Terrain t)
+    public override void InputControllerAwake(Terrain t)
     {
         
-    }
-
-    public static GameObject SpawnAlly (RaycastHit hit, 
-                                        ClashSpecies selected, Dictionary<int, int> remaining, ToggleGroup toggleGroup)
-    {
-        NavMeshHit placement;
-//        if (remaining[selected.id] < 1)
-//            return null;
-
-        if (NavMesh.SamplePosition (hit.point, out placement, 100, walkableAreaMask) && hit.collider.CompareTag ("Terrain")) {
-            Debug.Log ("mask is" + placement.mask);
-            var allyResource = Resources.Load<GameObject> ("Prefabs/ClashOfSpecies/Units/" + selected.name);
-            var allyObject = Instantiate (allyResource, placement.position, Quaternion.identity) as GameObject;
-            allyObject.tag = "Ally";
-
-            remaining [selected.id]--;
-            var toggle = toggleGroup.ActiveToggles ().FirstOrDefault ();
-            toggle.transform.parent.GetComponent<ClashUnitListItem> ().amountLabel.text = remaining [selected.id].ToString ();
-            if (remaining [selected.id] == 0) {
-                toggle.enabled = false;
-                toggle.interactable = false;
-//                selected = null;
-            }
-            return allyObject;
-        }
-        return null;
     }
 
     /// <summary>
@@ -92,9 +67,10 @@ public class COSTouchInputControler:InputControllerBase
     /// <param name="_camera">Camera.</param>
     /// <param name="eTouchRes">Current touch state</param>
     /// 
-    public override RaycastHit InputUpdate (Camera _camera)
+    public override RaycastHit InputUpdate(Camera _camera)
     {
-        if (updateZoomSensitivity) {
+        if (updateZoomSensitivity)
+        {
             moveSensitivityX = _camera.orthographicSize / 5.0f;
             moveSensitivityY = _camera.orthographicSize / 5.0f;
         }
@@ -102,15 +78,17 @@ public class COSTouchInputControler:InputControllerBase
         Touch[] touches = Input.touches;
 
 //        eTouchRes = COSTouchState.None;
-        RaycastHit hit = new RaycastHit ();
+        RaycastHit hit = new RaycastHit();
 
-        if (touches.Length < 1) {
+        if (touches.Length < 1)
+        {
             DetectTouchMovement.rotating = false;
             //if the camera is currently scrolling
-            if (scrollVelocity != 0.0f) {
+            if (scrollVelocity != 0.0f)
+            {
                 //slow down over time
                 float t = (Time.time - timeTouchPhaseEnded) / inertiaDuration;
-                float frameVelocity = Mathf.Lerp (scrollVelocity, 0.0f, t);
+                float frameVelocity = Mathf.Lerp(scrollVelocity, 0.0f, t);
                 Vector3 currTransform = -(Vector3)scrollDirection.normalized * (frameVelocity * 0.05f) * Time.deltaTime;
                 currTransform.y = 0;
                 _camera.transform.position += currTransform;
@@ -121,20 +99,27 @@ public class COSTouchInputControler:InputControllerBase
             eTouchRes = COSTouchState.None;
         }
 
-        if (touches.Length > 0) {
+        if (touches.Length > 0)
+        {
             //Single touch (move)
-            if (touches.Length == 1) {
+            if (touches.Length == 1)
+            {
                 DetectTouchMovement.rotating = false;
-                Ray ray = Camera.main.ScreenPointToRay (touches [0].position);
+                Ray ray = Camera.main.ScreenPointToRay(touches[0].position);
 //                RaycastHit hit;
-                if (Physics.Raycast (ray, out hit, 1000)) {
+                if (Physics.Raycast(ray, out hit, 1000))
+                {
                     //                    if (hit.collider.CompareTag("Terrain"))
                     NavMeshHit nmhPosition;
-                    if (NavMesh.SamplePosition (hit.point, out nmhPosition, 10, NavMesh.AllAreas)) {
-                        if (touches [0].phase == TouchPhase.Began) {
+                    if (NavMesh.SamplePosition(hit.point, out nmhPosition, 10, NavMesh.AllAreas))
+                    {
+                        if (touches[0].phase == TouchPhase.Began)
+                        {
                             oldTouchPos = hit.point;
                             eTouchRes = COSTouchState.None;
-                        } else if (touches [0].phase == TouchPhase.Moved && (hit.point - oldTouchPos).magnitude > minPanDistance) {
+                        }
+                        else if (touches[0].phase == TouchPhase.Moved && (hit.point - oldTouchPos).magnitude > minPanDistance)
+                        {
                             Vector3 delta = hit.point - oldTouchPos;
 
                             float positionX = delta.x * moveSensitivityX * Time.deltaTime;
@@ -143,10 +128,10 @@ public class COSTouchInputControler:InputControllerBase
                             float positionZ = delta.z * moveSensitivityY * Time.deltaTime;
                             positionZ = invertMoveY ? positionZ : positionZ * -1;
 
-                            _camera.transform.position += new Vector3 (positionX, 0, positionZ);
+                            _camera.transform.position += new Vector3(positionX, 0, positionZ);
 
                             scrollDirection = delta.normalized;
-                            scrollVelocity = touches [0].deltaPosition.magnitude / touches [0].deltaTime;
+                            scrollVelocity = touches[0].deltaPosition.magnitude / touches[0].deltaTime;
 
 
                             if (scrollVelocity <= 100)
@@ -154,13 +139,17 @@ public class COSTouchInputControler:InputControllerBase
 
 //                            isPanning = true;
                             eTouchRes = COSTouchState.IsPanning;
-                        } else if (touches [0].phase == TouchPhase.Ended) {
+                        }
+                        else if (touches[0].phase == TouchPhase.Ended)
+                        {
                             timeTouchPhaseEnded = Time.time;
-                            if (eTouchRes != COSTouchState.IsPanning) {
+                            if (eTouchRes != COSTouchState.IsPanning)
+                            {
                                 //here we handle simple tap on terrain
-                                Debug.Log ("tapped");
+                                Debug.Log("tapped");
                                 eTouchRes = COSTouchState.TerrainTapped;
-                            } else
+                            }
+                            else
                                 eTouchRes = COSTouchState.None;
                         }
                     }
@@ -168,13 +157,14 @@ public class COSTouchInputControler:InputControllerBase
             }
 
             //Double touch (zoom)
-            if (touches.Length == 2) {
-                Vector2 cameraViewsize = new Vector2 (_camera.pixelWidth, _camera.pixelHeight);
+            if (touches.Length == 2)
+            {
+                Vector2 cameraViewsize = new Vector2(_camera.pixelWidth, _camera.pixelHeight);
 
-                Touch touchOne = touches [0];
-                Touch touchTwo = touches [1];
+                Touch touchOne = touches[0];
+                Touch touchTwo = touches[1];
 
-                DetectTouchMovement.Calculate ();
+                DetectTouchMovement.Calculate();
 
                 Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
                 Vector2 touchTwoPrevPos = touchTwo.position - touchTwo.deltaPosition;
@@ -184,13 +174,16 @@ public class COSTouchInputControler:InputControllerBase
 
                 float deltaMagDiff = prevTouchDeltaMag - touchDeltaMag;
 
-                if (Mathf.Abs (DetectTouchMovement.pinchDistanceDelta) > 0) {
+                if (Mathf.Abs(DetectTouchMovement.pinchDistanceDelta) > 0)
+                {
                     _camera.fieldOfView += -DetectTouchMovement.pinchDistanceDelta * zoomSpeed;
                     // Clamp the field of view to make sure it's between minFOV and maxFOV.
-                    _camera.fieldOfView = Mathf.Clamp (_camera.fieldOfView, minFOV, maxFOV);
+                    _camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView, minFOV, maxFOV);
                     eTouchRes = COSTouchState.IsZooming;
-                } else {
-                    _camera.transform.RotateAround (_camera.transform.position, Vector3.up, 
+                }
+                else
+                {
+                    _camera.transform.RotateAround(_camera.transform.position, Vector3.up, 
                         -DetectTouchMovement.turnAngleDelta);
                     eTouchRes = COSTouchState.IsRotating;
                 }
