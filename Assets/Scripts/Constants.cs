c9c534344577fb9111244743cd77fce9b7145025
g.george.jone@gmail.com <g.george.jone@gmail.com>
2016-05-18 00:52:50 -0700

    resolving merge issues

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -10,7 +10,7 @@ public class Constants {
 	public static readonly string REMOTE_HOST = "localhost";
 	//public static readonly string REMOTE_HOST = "thecity.sfsu.edu";
 
-	public static readonly int REMOTE_PORT = 9255;//20041;
+	public static readonly int REMOTE_PORT = 9257;//20041;
 
 	public static readonly float HEARTBEAT_RATE = 1f;
 	
