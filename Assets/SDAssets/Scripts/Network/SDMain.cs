a24c3cb9b9f3a38f775fbeac5b0f92df3f9e7549
Rupal Khilari <rupal.khilari@gmail.com>
2016-05-19 02:05:01 -0700

    SDV - Fix in End game score.

--- Assets/SDAssets/Scripts/Network/SDMain.cs
+++ Assets/SDAssets/Scripts/Network/SDMain.cs
@@ -34,7 +34,6 @@ namespace SD
         }
 
         void OnDestroy() {
-            Debug.Log ("Thanks for playing Sea Divided.");
             StopCoroutine (RequestInGameHeartbeat());
         }
  
