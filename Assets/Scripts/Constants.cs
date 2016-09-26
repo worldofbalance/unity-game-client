685c1b14d6eacb89b084bf94f44867c8aec79fa0
David Hoff <dhoff@mail.sfsu.edu>
2016-09-26 14:52:44 -0700

    Use port 9255 for wob_wob server

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -18,11 +18,11 @@ public class Constants
     // IP address may be 130.212.3.51
 
     //public static readonly int REMOTE_PORT = 9234;
-    public static readonly int REMOTE_PORT = 9270;    // 9270 is new port as of 2016-9-13 - cvg account
+    // public static readonly int REMOTE_PORT = 9270;    // 9270 is new port as of 2016-9-13 - cvg account
     //20041;
     //    public static readonly int REMOTE_PORT = 9234;
     //9257 9255 for locahost, 9257 for Anu's server
-    // public static readonly int REMOTE_PORT = 9255;    // 9255  wob_wob account
+    public static readonly int REMOTE_PORT = 9255;    // 9255  wob_wob account
     public static readonly float HEARTBEAT_RATE = 1f;
 	
     // Other
