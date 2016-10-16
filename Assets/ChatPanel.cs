27c886898af83bfc81369d50935e0f207d0d8156
Jens Vanderhaeghe <jens.vanderhaeghe@gmail.com>
2016-10-16 16:54:22 -0700

    Created basic chat client

---
+++ Assets/ChatPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChatPanel : MonoBehaviour {
+
+	// Use this for initialization
+  Button btnSend;
+  InputField txtMessage;
+	void Start () {
+    btnSend = this.transform.Find("BtnSend").GetComponent<Button>();
+    txtMessage = this.transform.Find("TxtInput").GetComponent<InputField>();
+    btnSend.onClick.AddListener(sendChatMessage);
+	}
+
+  void sendChatMessage() {
+    Debug.Log(this.txtMessage.text);
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
